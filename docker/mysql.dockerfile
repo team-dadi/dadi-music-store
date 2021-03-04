e9f091a809ed2fe3b4822c5d27cbca2ec923980a
april-nguyen <aprilnguyen13@gmail.com>
2021-03-04 15:37:21 -0600

    add sql script to pre-populate database

--- docker/mysql.dockerfile
+++ docker/mysql.dockerfile
@@ -2,3 +2,4 @@ FROM mariadb:latest
 ENV MYSQL_ROOT_PASSWORD: root
 ENV MYSQL_USER: root
 ENV MYSQL_PASSWORD: root
+COPY *.sql /docker-entrypoint-initdb.d/
---
+++ sql-scripts/insert.sql
@@ -0,0 +1,43 @@
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Gabby Barrett');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Maren Morris');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Kendrick Lamar');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Eminem');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Diplo');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Beny Benassi');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Johann Strauss II');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Ludwig Van Beethoven');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('BTS');
+INSERT INTO `dadi`.`artist` (`name`) VALUES ('Adele'); 
+    
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('Carly Pearce', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('girl', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('rubber soul', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('Eminem', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('Purpose', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('Cinema', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('The Blue Danube', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('Fur Elise', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('Dynamite', '1');
+INSERT INTO `dadi`.`album` (`name`, `price`) VALUES ('21', '1');
+
+INSERT INTO `dadi`.`genre` (`name`) VALUES ('Country');
+INSERT INTO `dadi`.`genre` (`name`) VALUES ('Hip Hop');
+INSERT INTO `dadi`.`genre` (`name`) VALUES ('EDM');
+INSERT INTO `dadi`.`genre` (`name`) VALUES ('Classical');
+INSERT INTO `dadi`.`genre` (`name`) VALUES ('POP');
+
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('I hope you are happy now', '1', '1', '1');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('The Bones', '2', '2', '1');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Alls My Life', '3', '3', '2');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Lose Yourself', '4', '4', '2');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Where are you now', '5', '5', '3');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Cinema', '6', '6', '3');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('The Blue Danube', '7', '7', '4');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Fur Elise', '8', '8', '4');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Dynamite', '9', '9', '5');
+INSERT INTO `dadi`.`song` (`name`, `album_id`, `artist_id`, `genre_id`) VALUES ('Rolling in the deep', '10', '10', '5');
+    
+  
+  
+
+
