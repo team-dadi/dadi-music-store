50ccb667944cb10ca824d50bcee6f8cd35a45736
april-nguyen <aprilnguyen13@gmail.com>
2021-03-04 14:09:13 -0600

    add rest template bean and create shop controller #AB38

--- dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/DadiMusicWebappApplication.java
+++ dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/DadiMusicWebappApplication.java
@@ -2,6 +2,9 @@ package com.hcl.dadimusicwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DadiMusicWebappApplication {
@@ -10,4 +13,8 @@ public class DadiMusicWebappApplication {
 		SpringApplication.run(DadiMusicWebappApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
---
+++ dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/controller/ShopController.java
@@ -0,0 +1,98 @@
+package com.hcl.dadimusicwebapp.controller;
+
+import com.hcl.dadimusicwebapp.model.Album;
+import com.hcl.dadimusicwebapp.model.Artist;
+import com.hcl.dadimusicwebapp.model.Genre;
+import com.hcl.dadimusicwebapp.model.Song;
+import com.hcl.dadimusicwebapp.service.AlbumService;
+import com.hcl.dadimusicwebapp.service.ArtistService;
+import com.hcl.dadimusicwebapp.service.GenreService;
+import com.hcl.dadimusicwebapp.service.SongService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@Log4j2
+public class ShopController {
+  @Autowired
+  private SongService songService;
+
+  @Autowired
+  private GenreService genreService;
+
+  @Autowired
+  private AlbumService albumService;
+
+  @Autowired
+  private ArtistService artistService;
+
+  @GetMapping("/home")
+  public String index(ModelMap model) {
+    model.addAttribute("genreList", genreService.getAll());
+    return "index";
+  }
+
+  @GetMapping("/search")
+  public String search(ModelMap model) {
+    model.addAttribute("songList", songService.getAll());
+    model.addAttribute("genreList", genreService.getAll());
+
+    return "search";
+  }
+
+  @GetMapping("/search")
+  public String searchByAlbum(@RequestParam String albumName, ModelMap model) {
+    List<Album> albumList = albumService.getAll();
+    List<Song> songList = new ArrayList<Song>();
+    albumList.forEach(a -> {
+      if(a.getName().contains(albumName)) {
+        songList.addAll(songService.getByAlbumId(a.getId()));
+      }
+    } );
+    model.addAttribute("songList", songList);
+    return "search";
+  }
+
+  @GetMapping("/search")
+  public String searchByArtist(@RequestParam String artistName, ModelMap model) {
+    List<Artist> artistList = artistService.getAll();
+    List<Song> songList = new ArrayList<Song>();
+    artistList.forEach(a -> {
+      if(a.getName().contains(artistName)) {
+        //songList.addAll(songService.getByArtistId(a.getId()));
+      }
+    } );
+    model.addAttribute("songList", songList);
+    return "search";
+  }
+
+  @GetMapping("/search")
+  public String searchByTitle(@RequestParam String songName, ModelMap model) {
+    List<Song> allSongs = songService.getAll();
+    List<Song> songList = allSongs.stream().filter(
+      s -> s.getName().contains(songName))
+      .collect(Collectors.toList());
+    model.addAttribute("songList", songList);
+    return "search";
+  }
+
+  @GetMapping("/search?genreId={genreId}")
+  public String searchByGenre(@PathVariable int genreId, ModelMap model) {
+    List<Song> allSongs = songService.getAll();
+    List<Song> songList = allSongs.stream().filter(
+      s -> s.getGenre().getId() == genreId)
+      .collect(Collectors.toList());
+    model.addAttribute("songList", songList);
+    return "search";
+  }
+}
