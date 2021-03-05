cbc97aa8cca6a6e6e4309edb419f11e92201bc58
Indra Parameswaran <indrakumari.param@hcl.com>
2021-03-04 22:10:58 -0500

    AB#34 added object mapper to cartservice

--- dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/service/CartService.java
+++ dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/service/CartService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcl.dadimusicwebapp.model.Cart;
 
 @Service
@@ -23,7 +25,9 @@ public class CartService {
   public List<Cart> getAll() {
     String getAllUrl = url + "/all";
     log.debug("Sending get request to: " + getAllUrl);
-    List<Cart> cartList = restTemplate.getForObject(getAllUrl, List.class);
+    List<Cart> cartList = new ObjectMapper().convertValue(
+            restTemplate.getForObject(getAllUrl, List.class),
+            new TypeReference<List<Cart>>() { });
     return cartList;
   }
 
@@ -32,8 +36,6 @@ public class CartService {
     log.debug("Sending get request to: " + getUrl);
     return restTemplate.getForObject(getUrl, Cart.class);
   }
-
-
 
   public Cart add(Cart cart) {
     log.debug("Sending post request to: " + url);
@@ -50,4 +52,5 @@ public class CartService {
     log.debug("Sending delete request to: " + deleteUrl);
     restTemplate.delete(deleteUrl);
   }
+  
 }
