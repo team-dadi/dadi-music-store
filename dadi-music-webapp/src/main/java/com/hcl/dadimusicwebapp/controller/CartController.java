fe4b050349423e4fdc60265a82f30572a3259444
dmdijumathew22 <dmdijumathew22@gmail.com>
2021-03-05 11:23:26 -0500

    user checkout

--- dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/controller/CartController.java
+++ dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/controller/CartController.java
@@ -3,6 +3,7 @@ package com.hcl.dadimusicwebapp.controller;
 import com.hcl.dadimusicwebapp.model.Cart;
 import com.hcl.dadimusicwebapp.model.Invoice;
 import com.hcl.dadimusicwebapp.model.Song;
+import com.hcl.dadimusicwebapp.model.User;
 import com.hcl.dadimusicwebapp.service.CartService;
 import com.hcl.dadimusicwebapp.service.OrderService;
 import com.hcl.dadimusicwebapp.service.SongService;
@@ -11,6 +12,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,207 +30,216 @@ import java.util.List;
 @Controller
 @Log4j2
 public class CartController {
-  @Autowired
-  private CartService cartService;
+	@Autowired
+	private CartService cartService;
 
-  @Autowired
-  private SongService songService;
+	@Autowired
+	private SongService songService;
 
-  @Autowired
-  private UserService userService;
+	@Autowired
+	private UserService userService;
 
-  @Autowired
-  private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-  @GetMapping("/cart")
-  public String cart(ModelMap model, HttpServletRequest httpServletRequest) {
-    String username = httpServletRequest.getRemoteUser();
+	@GetMapping("/cart")
+	public String cart(ModelMap model, HttpServletRequest httpServletRequest) {
+		String username = httpServletRequest.getRemoteUser();
+		System.out.println("here---------->" + username);
+		log.debug(username);
 
-    if(username == null) {
-      Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
-      if(guestCart == null) {
-        guestCart = new Cart();
-        guestCart.setSongs(new ArrayList<Song>());
+		if (username == null) {
+			Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
+			if (guestCart == null) {
+				guestCart = new Cart();
+				guestCart.setSongs(new ArrayList<Song>());
 
-        httpServletRequest.getSession(true)
-          .setAttribute("cart", guestCart );
-      }
-      model.addAttribute("songList", guestCart.getSongs());
-    } else {
-      Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
-      model.addAttribute("songList", userCart.getSongs());
-    }
+				httpServletRequest.getSession(true).setAttribute("cart", guestCart);
+			}
+			model.addAttribute("songList", guestCart.getSongs());
+		} else {
 
-    return "cart";
-  }
+			Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
+			model.addAttribute("songList", userCart.getSongs());
+		}
 
-  @GetMapping("/add/{id}")
-  public ResponseEntity addSong(@PathVariable int id, HttpServletRequest httpServletRequest) {
-    String username = httpServletRequest.getRemoteUser();
-    Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
-    Song song = songService.getById(id);
+		return "cart";
+	}
 
-    if(song == null) {
-      return new ResponseEntity<String>("Couldn't find song with :" + id, HttpStatus.NOT_FOUND);
-    }
+	@GetMapping("/add/{id}")
+	public ResponseEntity addSong(@PathVariable int id, HttpServletRequest httpServletRequest) {
+		String username = httpServletRequest.getRemoteUser();
+		Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
+		Song song = songService.getById(id);
 
-    log.debug("Cart is adding song: " + song);
+		if (song == null) {
+			return new ResponseEntity<String>("Couldn't find song with :" + id, HttpStatus.NOT_FOUND);
+		}
 
-    if (guestCart == null && username == null) {        // not logged in, no guest cart (new)
-      Cart newCart = new Cart();
-      newCart.setSongs(new ArrayList<Song>());
-      newCart.getSongs().add(song);
+		log.debug("Cart is adding song: " + song);
 
-      httpServletRequest.getSession(true)
-        .setAttribute("cart", newCart );
-    } else if(guestCart != null && username != null) {   // is logged in, has guest cart (transfer, remove guest cart)
-      Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
+		if (guestCart == null && username == null) { // not logged in, no guest cart (new)
+			Cart newCart = new Cart();
+			newCart.setSongs(new ArrayList<Song>());
+			newCart.getSongs().add(song);
 
-      if (userCart.getSongs() == null) {
-        userCart.setSongs(new ArrayList<Song>());
-      }
-      userCart.getSongs().addAll(guestCart.getSongs());
-      userCart.getSongs().add(song);
-      cartService.update(userCart);
+			httpServletRequest.getSession(true).setAttribute("cart", newCart);
+		} else if (guestCart != null && username != null) { // is logged in, has guest cart (transfer, remove guest
+															// cart)
+			Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
 
-      httpServletRequest.removeAttribute("cart");
-    } else if(username != null) {                     // user logged in, no guest cart
-      Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
-      if (userCart.getSongs() == null) {
-        userCart.setSongs(new ArrayList<Song>());
-      }
-      userCart.getSongs().add(song);
-      cartService.update(userCart);
-    } else {                                        // not logged in, has guest cart (add)
-      guestCart.getSongs().add(song);
-      httpServletRequest.getSession()
-        .setAttribute("cart", guestCart );
-    }
-    return new ResponseEntity<String>("Song added to cart successfully", HttpStatus.OK);
-  }
+			if (userCart.getSongs() == null) {
+				userCart.setSongs(new ArrayList<Song>());
+			}
+			userCart.getSongs().addAll(guestCart.getSongs());
+			userCart.getSongs().add(song);
+			cartService.update(userCart);
 
-  @GetMapping("/delete/{id}")
-  public ResponseEntity deleteSong(@PathVariable int id, HttpServletRequest httpServletRequest) {
-    String username = httpServletRequest.getRemoteUser();
-    Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
-    Song song = songService.getById(id);
+			httpServletRequest.removeAttribute("cart");
+		} else if (username != null) { // user logged in, no guest cart
+			Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
+			if (userCart.getSongs() == null) {
+				userCart.setSongs(new ArrayList<Song>());
+			}
+			userCart.getSongs().add(song);
+			cartService.update(userCart);
+		} else { // not logged in, has guest cart (add)
+			guestCart.getSongs().add(song);
+			httpServletRequest.getSession().setAttribute("cart", guestCart);
+		}
+		return new ResponseEntity<String>("Song added to cart successfully", HttpStatus.OK);
+	}
 
-    if(song == null) {
-      return new ResponseEntity<String>("Couldn't find song with :" + id, HttpStatus.NOT_FOUND);
-    }
+	@GetMapping("/delete/{id}")
+	public ResponseEntity deleteSong(@PathVariable int id, HttpServletRequest httpServletRequest) {
+		String username = httpServletRequest.getRemoteUser();
+		Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
+		Song song = songService.getById(id);
 
-    log.debug("Cart is removing song: " + song);
+		if (song == null) {
+			return new ResponseEntity<String>("Couldn't find song with :" + id, HttpStatus.NOT_FOUND);
+		}
 
-    if (guestCart == null && username == null) {        // not logged in, no guest cart (error)
-      return new ResponseEntity<String>("Nothing in cart to delete" + id, HttpStatus.NOT_FOUND);
-    } else if(guestCart != null && username != null) {   // is logged in, has guest cart (transfer, remove guest cart)
-      Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
+		log.debug("Cart is removing song: " + song);
 
-      if (userCart.getSongs() == null) {
-        userCart.setSongs(new ArrayList<Song>());
-      }
-      userCart.getSongs().addAll(guestCart.getSongs());
-      if(userCart.getSongs().contains(song)) {
-        userCart.getSongs().remove(song);
-      } else {
-        return new ResponseEntity<String>("Cannot delete song that is not in cart" + id, HttpStatus.NOT_FOUND);
-      }
+		if (guestCart == null && username == null) { // not logged in, no guest cart (error)
+			return new ResponseEntity<String>("Nothing in cart to delete" + id, HttpStatus.NOT_FOUND);
+		} else if (guestCart != null && username != null) { // is logged in, has guest cart (transfer, remove guest
+															// cart)
+			Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
 
-      cartService.update(userCart);
+			if (userCart.getSongs() == null) {
+				userCart.setSongs(new ArrayList<Song>());
+			}
+			userCart.getSongs().addAll(guestCart.getSongs());
+			if (userCart.getSongs().contains(song)) {
+				userCart.getSongs().remove(song);
+			} else {
+				return new ResponseEntity<String>("Cannot delete song that is not in cart" + id, HttpStatus.NOT_FOUND);
+			}
 
-      httpServletRequest.removeAttribute("cart");
-    } else if(username != null) {                     // user logged in, no guest cart
-      Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
-      if (userCart.getSongs() == null) {
-        return new ResponseEntity<String>("Nothing in cart to delete" + id, HttpStatus.NOT_FOUND);
-      }
+			cartService.update(userCart);
 
-      if(userCart.getSongs().contains(song)) {
-        userCart.getSongs().remove(song);
-        cartService.update(userCart);
-      } else {
-        return new ResponseEntity<String>("Cannot delete song that is not in cart" + id, HttpStatus.NOT_FOUND);
-      }
+			httpServletRequest.removeAttribute("cart");
+		} else if (username != null) { // user logged in, no guest cart
+			Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
+			if (userCart.getSongs() == null) {
+				return new ResponseEntity<String>("Nothing in cart to delete" + id, HttpStatus.NOT_FOUND);
+			}
 
-    } else {                                        // not logged in, has guest cart (add)
-      if(guestCart.getSongs().contains(song)) {
-        guestCart.getSongs().remove(song);
-        httpServletRequest.getSession()
-          .setAttribute("cart", guestCart );
-      } else {
-        return new ResponseEntity<String>("Cannot delete song that is not in cart" + id, HttpStatus.NOT_FOUND);
-      }
-    }
-    return new ResponseEntity<String>("Song delete from cart successfully", HttpStatus.OK);
-  }
+			if (userCart.getSongs().contains(song)) {
+				userCart.getSongs().remove(song);
+				cartService.update(userCart);
+			} else {
+				return new ResponseEntity<String>("Cannot delete song that is not in cart" + id, HttpStatus.NOT_FOUND);
+			}
 
-  @GetMapping("/checkout")
-  public String checkout(ModelMap model, HttpServletRequest httpServletRequest, RedirectAttributes attributes) {
-    String username = httpServletRequest.getRemoteUser();
+		} else { // not logged in, has guest cart (add)
+			if (guestCart.getSongs().contains(song)) {
+				guestCart.getSongs().remove(song);
+				httpServletRequest.getSession().setAttribute("cart", guestCart);
+			} else {
+				return new ResponseEntity<String>("Cannot delete song that is not in cart" + id, HttpStatus.NOT_FOUND);
+			}
+		}
+		return new ResponseEntity<String>("Song delete from cart successfully", HttpStatus.OK);
+	}
 
-    if(username == null) {
-      Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
-      if(guestCart == null) {
-        guestCart = new Cart();
-        guestCart.setSongs(new ArrayList<Song>());
+	@GetMapping("/checkout")
+	public String checkout(ModelMap model, HttpServletRequest httpServletRequest, RedirectAttributes attributes) {
+		String username = httpServletRequest.getRemoteUser();
 
-        httpServletRequest.getSession(true)
-          .setAttribute("cart", guestCart );
-        attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
-        return "redirect:/cart";
-      } else if(guestCart.getSongs().isEmpty() || guestCart.getSongs() == null) {
-        attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
-        return "redirect:/cart";
-      }
-      model.addAttribute("songList", guestCart.getSongs());
-    } else {
-      Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
-      model.addAttribute("songList", userCart.getSongs());
-    }
+		if (username == null) {
+			Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
+			if (guestCart == null) {
+				guestCart = new Cart();
+				guestCart.setSongs(new ArrayList<Song>());
 
-    return "checkout";
-  }
+				httpServletRequest.getSession(true).setAttribute("cart", guestCart);
+				attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
+				return "redirect:/cart";
+			} else if (guestCart.getSongs().isEmpty() || guestCart.getSongs() == null) {
+				attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
+				return "redirect:/cart";
+			}
+			model.addAttribute("songList", guestCart.getSongs());
+		} else {
+			Cart userCart = cartService.getById(userService.findByUsername(username).getCart().getId());
+			model.addAttribute("songList", userCart.getSongs());
+		}
 
-  @PostMapping("/checkout")
-  public String confirmOrder(ModelMap model, HttpServletRequest httpServletRequest, RedirectAttributes attributes) {
-    String username = httpServletRequest.getRemoteUser();
+		return "checkout";
+	}
 
-    if(username == null) {
-      Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
-      if(guestCart == null) {
-        guestCart = new Cart();
-        guestCart.setSongs(new ArrayList<Song>());
+	@PostMapping("/checkout")
+	public String confirmOrder(ModelMap model, HttpServletRequest httpServletRequest, RedirectAttributes attributes) {
+		String username = httpServletRequest.getRemoteUser();
 
-        httpServletRequest.getSession(true)
-          .setAttribute("cart", guestCart );
-        attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
-        return "redirect:/cart";
-      } else if(guestCart.getSongs().isEmpty() || guestCart.getSongs() == null) {
-        attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
-        return "redirect:/cart";
-      }
+		if (username == null) {
+			Cart guestCart = (Cart) httpServletRequest.getSession().getAttribute("cart");
+			if (guestCart == null) {
+				guestCart = new Cart();
+				guestCart.setSongs(new ArrayList<Song>());
 
-      List<Song> purchase = guestCart.getSongs();
-      httpServletRequest.getSession().removeAttribute("cart");
+				httpServletRequest.getSession(true).setAttribute("cart", guestCart);
+				attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
+				return "redirect:/cart";
+			} else if (guestCart.getSongs().isEmpty() || guestCart.getSongs() == null) {
+				attributes.addFlashAttribute("cartAlert", "Nothing in Cart to Checkout!");
+				return "redirect:/cart";
+			}
 
-      Invoice guestInvoice = new Invoice();
-      guestInvoice.setPurchased(purchase);
+			List<Song> purchase = guestCart.getSongs();
+			httpServletRequest.getSession().removeAttribute("cart");
 
-      Invoice addedInvoice = orderService.add(guestInvoice);
-      model.addAttribute("invoice", addedInvoice);
-    } else {
-      List<Song> userPurchases = cartService.getById(userService.findByUsername(username).getCart().getId()).getSongs();
+			Invoice guestInvoice = new Invoice();
+			guestInvoice.setPurchased(purchase);
 
-      Invoice guestInvoice = new Invoice();
-      guestInvoice.setPurchased(userPurchases);
+			Invoice addedInvoice = orderService.add(guestInvoice);
+			model.addAttribute("invoice", addedInvoice);
+		} else {
+			List<Song> userPurchases = cartService.getById(userService.findByUsername(username).getCart().getId())
+					.getSongs();
 
-      Invoice addedInvoice = orderService.add(guestInvoice);
-      model.addAttribute("invoice", addedInvoice);
+			Invoice guestInvoice = new Invoice();
+			guestInvoice.setPurchased(userPurchases);
 
-      int id = userService.findByUsername(username).getCart().getId();
-      cartService.delete(id);
-    }
+			Invoice addedInvoice = orderService.add(guestInvoice);
+			model.addAttribute("invoice", addedInvoice);
 
-    return "invoice";
-  }
+			int id = userService.findByUsername(username).getCart().getId();
+			Invoice invoice= new Invoice();
+			invoice.setId(addedInvoice.getId());
+			User user = userService.findByUsername(username);
+			user.getInvoices().add(invoice);
+			userService.addUser(user);
+			Cart cart=new Cart();
+			cart.setId(id);
+			cart.setSongs(new ArrayList <Song>());
+			
+			cartService.update(cart);
+		}
+
+		return "invoice";
+	}
 }
--- dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/service/UserService.java
+++ dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.hcl.dadimusicwebapp.model.Cart;
 import com.hcl.dadimusicwebapp.model.User;
 
 @Service
@@ -28,6 +29,9 @@ public class UserService {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private CartService cartService;
 
 	public User findByUsername(String username) {
 		String url = findUrl + username;
@@ -39,10 +43,13 @@ public class UserService {
 
 	public User addUser(User account) {
 		User user = new User();
+		Cart cart = cartService.add(new Cart());
+		
 		user.setUsername(account.getUsername());
 		user.setPassword(bCryptPasswordEncorder.encode(account.getPassword()));
 		user.setEmail(account.getEmail());
 		user.setRole("ROLE_USER");
+		user.setCart(cart);
 
 		log.debug("Sending post request to " + addUrl + " to add user: " + user);
 
