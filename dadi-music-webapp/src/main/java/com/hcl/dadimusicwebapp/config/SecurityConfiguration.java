3469bed2ab2ddb37f61acadb3d04b809bf4b3160
april-nguyen <aprilnguyen13@gmail.com>
2021-03-05 07:09:23 -0600

    update security config

--- dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/config/SecurityConfiguration.java
+++ dadi-music-webapp/src/main/java/com/hcl/dadimusicwebapp/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
       .authorizeRequests()
-        .antMatchers("/", "/search**", "/cart", "/register").permitAll()
+        .antMatchers("/", "/search**", "/cart", "/register", "/add/**", "/delete/**").permitAll()
         .anyRequest().authenticated()
         .and()
       .formLogin()
