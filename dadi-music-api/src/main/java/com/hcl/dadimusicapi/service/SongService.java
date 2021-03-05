d55bd41118792732eb9a8d725e4145aaed2237d5
dmdijumathew22 <dmdijumathew22@gmail.com>
2021-03-04 22:08:17 -0500

    find by artist in songservice AB#11

--- dadi-music-api/src/main/java/com/hcl/dadimusicapi/service/SongService.java
+++ dadi-music-api/src/main/java/com/hcl/dadimusicapi/service/SongService.java
@@ -2,6 +2,7 @@ package com.hcl.dadimusicapi.service;
 
 import com.hcl.dadimusicapi.exception.NotFoundException;
 import com.hcl.dadimusicapi.model.Album;
+import com.hcl.dadimusicapi.model.Artist;
 import com.hcl.dadimusicapi.model.Song;
 import com.hcl.dadimusicapi.repo.SongRepo;
 import lombok.extern.log4j.Log4j2;
@@ -13,43 +14,48 @@ import java.util.List;
 @Service
 @Log4j2
 public class SongService {
-  @Autowired
-  private SongRepo songRepo;
+	@Autowired
+	private SongRepo songRepo;
 
-  public List<Song> getAll() {
-    log.debug("Getting all songs");
-    return songRepo.findAll();
-  }
+	public List<Song> getAll() {
+		log.debug("Getting all songs");
+		return songRepo.findAll();
+	}
 
-  public List<Song> findByAlbum(Album album) {
-    log.debug("Finding songs with album: " + album);
-    return songRepo.findAllByAlbum(album);
-  }
+	public List<Song> findByAlbum(Album album) {
+		log.debug("Finding songs with album: " + album);
+		return songRepo.findAllByAlbum(album);
+	}
 
-  public Song findById(int id) {
-    log.debug("Finding song with id: " + id);
-    return songRepo.findById(id).orElseThrow(() -> new NotFoundException("Song not found!"));
-  }
+	public List<Song> findByArtist(Artist artist) {
+		log.debug("Finding songs with artist: " + artist);
+		return songRepo.findAllByArtist(artist);
+	}
 
-  public Song add(Song song) {
-    log.debug("Adding song: " + song);
-    return songRepo.save(song);
-  }
+	public Song findById(int id) {
+		log.debug("Finding song with id: " + id);
+		return songRepo.findById(id).orElseThrow(() -> new NotFoundException("Song not found!"));
+	}
 
-  public Song update(Song song) {
-    if(!songRepo.existsById(song.getId())) {
-      throw new NotFoundException("Cannot update song as song does not exist!");
-    }
-    log.debug("Updating song: " + song);
-    return songRepo.save(song);
-  }
+	public Song add(Song song) {
+		log.debug("Adding song: " + song);
+		return songRepo.save(song);
+	}
 
-  public void delete(int id) {
-    if(!songRepo.existsById(id)){
-      log.debug("Invalid Song Id: Song with id # " + id + "does not exist!");
-      throw new NotFoundException("Cannot delete song as song does not exist!");
-    }
-    log.debug("Deleting song with id: " + id);
-    songRepo.deleteById(id);
-  }
+	public Song update(Song song) {
+		if (!songRepo.existsById(song.getId())) {
+			throw new NotFoundException("Cannot update song as song does not exist!");
+		}
+		log.debug("Updating song: " + song);
+		return songRepo.save(song);
+	}
+
+	public void delete(int id) {
+		if (!songRepo.existsById(id)) {
+			log.debug("Invalid Song Id: Song with id # " + id + "does not exist!");
+			throw new NotFoundException("Cannot delete song as song does not exist!");
+		}
+		log.debug("Deleting song with id: " + id);
+		songRepo.deleteById(id);
+	}
 }
